6eb940e31b6a5b2a137f8f775bcf86e1a29d502a
Hoàng Nam Hải <147267934+Hoang-namhai@users.noreply.github.com>
2025-05-23 11:49:41 +0700

    Add files via upload

---
+++ Summoner Hero.lua
@@ -0,0 +1,151 @@
+
+
+
+local n, startAddress, endAddress = nil, 0, 0
+local function name(lib)
+	if n == lib then
+		return startAddress, endAddress
+	end
+	local ranges = gg.getRangesList(lib or 'libil2cpp.so')
+	for i, v in ipairs(ranges) do
+		if v.state == "Xa" then
+			startAddress = v.start
+			endAddress = ranges[#ranges]['end']
+			break
+		end
+	end
+	return startAddress, endAddress
+end
+
+local function setHexMemory(libname, offset, hex)
+	name(libname)
+	local t, total = {}, 0
+	for h in string.gmatch(hex, "%S%S") do
+	    table.insert(t, {
+	        address = startAddress + offset + total,
+	        flags = gg.TYPE_BYTE,
+	        value = h .. "r"
+	    })
+	    total = total + 1
+	end
+	local res = gg.setValues(t)
+	if type(res) ~= 'string' then
+		return true
+	else
+		gg.alert(res)
+		return false
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- MENU--
+-- local isHide = false
+function Main()
+HNHH = gg.choice({
+"➣ OneHit GodMod😎 ",
+"[𝙴𝚇𝙸𝚃]"
+}, nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮G𝙰𝙼𝙴: 𝚂𝚄𝙼𝙼𝙾𝙽𝙴𝚁 𝙷𝙴𝚁𝙾\n💭V𝙴𝚁𝚂𝙸𝙾𝙽: 1.0.5 \n------------------------------------------------")
+if HNHH==1 then F1() end
+if HNHH==2 then EXITALL() end
+XGCK = -1
+end
+
+
+function F1()
+function searchValue(t,hai1,hai2)
+gg.clearResults()
+gg.clearList()
+rt={}
+gg.setRanges(hai1)
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(999999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+local input = gg.prompt(
+	{'🔰Điền Số Máu Của Hero Vào Đây🔰:'},
+	{[1] = "☛☚"},
+	{[1] = 'number'})
+if input == nil then gg.toast("🔥Đᴀ̃ Hᴜ̉ʏ🔥") MenuBattle() os.exit() end				
+r=searchValue({input[1],{input[1],1*4}},32,16)
+if #r == 0 then else
+searchEdit(0*4,16, 999999999)
+searchEdit(1*4,16, 999999999)
+gg.clearResults()
+end
+
+setHexMemory("libil2cpp.so", 0x2876d70, "20 2B A9 D2 00 70 80 F2 00 00 27 1E C0 03 5F D6")
+setHexMemory("libil2cpp.so", 0x2875f4c, "20 2B A9 D2 00 70 80 F2 00 00 27 1E C0 03 5F D6")
+setHexMemory("libil2cpp.so", 0x287683c, "20 2B A9 D2 00 70 80 F2 00 00 27 1E C0 03 5F D6")
+gg.toast('✅𝙰𝙲𝚃𝙸𝚅𝙰𝚃𝙴𝙳✅')
+end
+
+
+
+
+
+function EXITALL()
+gg.toast('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK == 1 then
+Main()
+end
+end
