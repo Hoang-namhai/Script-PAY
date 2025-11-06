2d6cf16ffff15a3e23e51933920dfb800b338b28
Hoàng Nam Hải <147267934+Hoang-namhai@users.noreply.github.com>
2025-11-06 08:32:29 +0700

    Update ĐHCD All Vip.lua

--- ĐHCD All Vip.lua
+++ ĐHCD All Vip.lua
@@ -1,3 +1,643 @@
-gg.alert("༒Script Đã Có Bản Update Mới༒\n➤ Liên Hệ Admin Để Cấp File 📄 Mới Nha...\n\n➤ Zalo: 0375574755📲...")
+local keyFile = "/storage/emulated/0/Android/LoadKey.txt" --
+local Passwords = {
+"0609",
+"TheVinh123",
+"Phuoc1234",
+"KhanhAn123",
+"KhanhAn1234"	
+  
+  
+  
+  
+  
+
+  
+
+  
+  
+  
+  
+  
+}
+function isValidKey(inputKey)
+  for _, v in ipairs(Passwords) do
+    if inputKey == v then
+      return true
+    end
+  end
+  return false
+end
+function readSavedKey()
+  local file = io.open(keyFile, "r")
+  if file then
+    local savedKey = file:read("*a")
+    file:close()
+    return savedKey
+  end
+  return nil
+end
+function saveKey(k)
+  local file = io.open(keyFile, "w")
+  if file then
+    file:write(k)
+    file:close()
+  end
+end
+local savedKey = readSavedKey()
+if savedKey and isValidKey(savedKey) then
+gg.alert("🎉Lᴏɢɪɴ Tʜᴀ̀ɴʜ Cᴏ̂ɴɢ🎉") 
+  
+else
+  local input = gg.prompt({"🔑 Lᴏᴀᴅ Kᴇʏ:"}, nil, {"text"})
+  if not input or not isValidKey(input[1]) then
+    gg.alert("❌ Kᴇʏ Sᴀɪ. Hᴏᴀ̣̆ᴄ Đᴀ̃ Qᴜᴀ́ Tʜɪᴇ̂́ᴛ Bɪ̣ Sᴜ̛̉ Dᴜ̣ɴɢ!")
+    gg.copyText("0375574755")
+    gg.alert("📲 Đã copy SĐT Admin!\nVào Zalo → Dán → Tìm kiếm để liên hệ Admin cấp key mới.")
+    os.exit()
+  else
+    saveKey(input[1])
+    gg.alert("🎉Kᴇʏ ᴆᴜ́ɴɢ. Lᴏɢɪɴ Tʜᴀ̀ɴʜ Cᴏ̂ɴɢ🎉") end
+end
+
+
+
+
+
+gg.sleep(100) 
+gg.getTargetInfo()
+gg.setVisible(false)
+KingModVN = os.date ("\
+۞•──────✧❅✦❅✧──────•۞\
+\
+📆Dᴀᴛᴇ📆 : %d/%m/%Y\
+\
+🕛Tɪᴍᴇ🕛 : %H:%M:%S\
+\
+💢Dᴠɪᴄᴇ Oɴʟɪɴᴇ💢 : 𝟣 ᴛʜɪᴇ̂́ᴛ ʙɪ̣\
+\
+⏳Lᴀꜱᴛ Uᴘᴅᴀᴛᴇ⏳ : 25/07/2025\
+\
+🔥Mᴀᴅᴇ Bʏ Hᴏᴀ̀ɴɢ Nᴀᴍ Hᴀ̉ɪ🔥\
+\
+🔰Sᴄʀɪᴘᴛ Pᴜʙʟɪsʜᴇᴅ ɪɴ ©2025🔰\
+\
+۞•──────✧❅✦❅✧──────•۞")
+gg.alert(KingModVN)
+gg.setVisible(true)
+
+gg.sleep(50)
+gg.sleep(50)
+
+
+
+
+HDR = "❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮G𝙰𝙼𝙴: 𝙳𝙷𝙲𝙳 𝙰𝙻𝙻 𝚅𝙸𝙿\n💭V𝙴𝚁𝚂𝙸𝙾𝙽: 𝚅𝙸𝙿 𝙿𝚁𝙾 \n-------------------------------------------"
+
+
+
+
+
+
+
+function Menu()
+local menu=gg.choice({
+"༒Damage Vip [ Load ]༒",
+"༒Time End [ Trận ]༒",
+"༒Skill Vip [ Trận ]༒",
+"༒Bí Cảnh [ Trận ]༒",
+"༒Gia Tộc [ Trận ]༒",
+"༒Ngư Ca Đàm [ Sảnh ]༒",
+"༒Speed X2 [ Sảnh ]༒",
+"༒[ Hướng Dẫn SD ]༒",
+"༒Zalo ADMIN༒",
+"Exit",
+},nil,HDR)
+if menu==nil then else
+if menu==1 then F1() end
+if menu==2 then F2() end
+if menu==3 then F3() end
+if menu==4 then F4() end
+if menu==5 then F5() end
+if menu==6 then F6() end
+if menu==7 then F7() end
+if menu==8 then F8() end
+if menu==9 then F9() end
+if menu==10 then os.exit() end
+--❏━━━━━━━━━━━━━━❒
+end
+SMZ=-1
+end
+--❏━━━━━━━━━━━━━━❒
+--gg.TYPE_DWORD = 4
+--gg.TYPE_FLOAT = 16
+--gg.TYPE_DOUBLE = 64
+--gg.TYPE_BYTE = 1
+--gg.TYPE_WORD = 2
+--gg.TYPE_QWORD = 32
+--❏━━━━━━━━━━━━━━❒
+function F1()
+clear() X="MonsterCfg" O=0x30 T=4 class()
+X="2~999999" refine()
+check() if E==0 then error() return end
+X=-1 edit() clear()
+
+clear() X="MonsterCfg" O=0x40 T=4 class()
+X="2~999999" refine()
+check() if E==0 then error() return end
+X=0 edit() clear()
+gg.toast('✅OneHit✅')
+end
+
+
+
+
+
+
+
+function F2()
+clear() X="LevelMonsterMgr" O=0x54 T=4 class()
+X="1~300" refine()
+check() if E==0 then error() return end
+X=800 edit() clear()
+gg.toast('✅Time End✅')
+end
+
+
+
+
+
+
+
+function F3()
+clear() X="SkillRuleEventSelectRandomEnemy" O=0x3C T=4 class()
+X="1" refine()
+check() if E==0 then error() return end
+X=25 edit() 
+local t = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.addListItems(t)
+t = nil
+local copy = false
+local t = gg.getListItems()
+if not copy then gg.removeListItems(t) end
+for i, v in ipairs(t) do
+	v.address = v.address + 0xffffffffffffffd8
+	if copy then v.name = v.name..' #2' end
+end
+gg.addListItems(t)
+t = nil
+copy = nil
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "-88888888"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearList()
+local t = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+local copy = false
+local t = gg.getListItems()
+if not copy then gg.removeListItems(t) end
+for i, v in ipairs(t) do
+	v.address = v.address + 0xffffffffffffffec
+	if copy then v.name = v.name..' #2' end
+end
+gg.addListItems(t)
+t = nil
+copy = nil
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "88888888"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearList()
+clear()
+gg.toast('✅Skill Vip✅')
+end
+
+
+
+
+
+
+
+function F4()
+local HNH4 = gg.alert('❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n--------------------------------------------\n\n➤ 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n[༒Bí Cảnh༒]\n\n➤ Lưu Ý:\n➤ Chức Năng Bật Trong Trận Nhé...\n➤ Combo1 Là X1500 Liên Tục...\n➤ Còn Combo2 Là Giữ Liên Trảm Combo Liên Tục Nhé ', '[🔰Combo1🔰]', '[🔰Combo2🔰]')
+if HNH4 == nil then return 
+else
+if HNH4 == 1 then ONBC1() end
+if HNH4 == 2 then ONBC2() end
+end
+::ni::
+end
+
+
+
+function ONBC1()
+clear() X="BaizhanSecretScoreDataMgr" O=0x38 T=4 class()
+check() if E==0 then error() return end
+gg.getResults(99999)
+revert = gg.getResults(8888)
+local t = gg.getResults(8888)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "1500"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('✅𝙲𝙾𝙼𝙱𝙾1✅') clear()
+end
+
+
+
+function ONBC2()
+clear() X="BaizhanSecretScoreDataMgr" O=0x4C T=4 class()
+check() if E==0 then error() return end
+gg.getResults(99999)
+revert = gg.getResults(8888)
+local t = gg.getResults(8888)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "8888"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('✅𝙲𝙾𝙼𝙱𝙾2✅') clear()
+end
+
+
+
+
+
+function F5()
+local HNH5 = gg.alert('❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n--------------------------------------------\n\n➤ 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n[༒Bí Cảnh༒]\n\n➤ Lưu Ý:\n➤ Chức Năng Bật Trong Trận Nhé...\n➤ Combo1 Là X1500 Liên Tục...\n➤ Còn Combo2 Là Giữ Liên Trảm Combo Liên Tục Nhé ', '[🔰Combo1🔰]', '[🔰Combo2🔰]')
+if HNH5 == nil then return 
+else
+if HNH5 == 1 then ONGT1() end
+if HNH5 == 2 then ONGT2() end
+end
+::ni::
+end
+
+
+
+function ONGT1()
+clear() X="JiaZuLianSaiScoreDataMgr" O=0x38 T=4 class()
+check() if E==0 then error() return end
+gg.getResults(99999)
+revert = gg.getResults(8888)
+local t = gg.getResults(8888)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "1500"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('✅𝙲𝙾𝙼𝙱𝙾1✅') clear()
+end
+
+
+
+function ONGT2()
+clear() X="JiaZuLianSaiScoreDataMgr" O=0x4C T=4 class()
+check() if E==0 then error() return end
+gg.getResults(99999)
+revert = gg.getResults(8888)
+local t = gg.getResults(8888)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "8888"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('✅𝙲𝙾𝙼𝙱𝙾2✅') clear()
+end
+
+
+
+
+
+function F6()
+local input = gg.prompt(
+	{"♥Ytb :: Hải Script🍀\n\nLevel Value♛:"},
+{[1] = "LVL1;   LVL2;   LVL3;   LVL4;       \n 6619252;3014771;6422625:113"})
+if input == nil then gg.toast("🔥Đᴀ̃ Hᴜ̉ʏ🔥") os.exit() end
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(input[1],gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("1~59", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+revert = gg.getResults(8888)
+local t = gg.getResults(8888)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "60"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('✅𝙽𝙶𝚄 𝙲𝙰 𝙳𝙰𝙼✅')
+end
+
+
+
+
+
+function F7()
+local HNH2 = gg.alert('❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n--------------------------------------------\n\n➤ 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n[™𝚂𝙿𝙴𝙴𝙳 𝚇𝟸™]💦\n\n➤ Lưu Ý:\n➤ Chức Năng Bật Trong Trận Hay Sảnh Đều Được.\n➤ Nhớ Là Phải Bật Chức Năng Speed Sau Nhất Nhé.', '[🔰ON🔰]', '[🔰OFF🔰]')
+if HNH2 == nil then return 
+else
+if HNH2 == 1 then ONSpeed() end
+if HNH2 == 2 then OFFSpeed() end
+end
+::ni::
+end
+
+function ONSpeed()
+function searchValue(t,hai1,hai2)
+gg.clearResults()
+gg.clearList()
+rt={}
+gg.setRanges(hai1)
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(9)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+tt[ii].freeze = true
+tt[ii].freezeType = gg.FREEZE_NORMAL
+gg.addListItems(tt)
+t = nil
+end
+gg.setValues(tt)
+end end
+r=searchValue({"4392630932057270955",{"4392630932057270955",0*4}},32,32)
+if #r == 0 then else
+searchEdit(-1*4,16, 2)
+gg.clearResults()
+gg.alert('✅𝚂𝙿𝙴𝙴𝙳 𝚇𝟸✅')
+end
+end
+
+function OFFSpeed()
+function searchValue(t,hai1,hai2)
+gg.clearResults()
+gg.clearList()
+rt={}
+gg.setRanges(hai1)
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(9)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+tt[ii].freeze = true
+tt[ii].freezeType = gg.FREEZE_NORMAL
+gg.addListItems(tt)
+t = nil
+end
+gg.setValues(tt)
+end end
+r=searchValue({"4392630932057270955",{"4392630932057270955",0*4}},32,32)
+if #r == 0 then else
+searchEdit(-1*4,16, 1)
+gg.clearList()
+gg.clearResults()
+gg.alert('❌𝚂𝙿𝙴𝙴𝙳 𝚇𝟸❌')
+end
+end
+
+
+
+function F8()
+gg.alert("1. ༒Damage Vip ༒\n[ Chức Năng Này Tùy Phiên Bản Mà AE Sử Dụng, Bắt Đầu Thì AE Vào 1 Trận Đấu Bất Kỳ Rồi Đợi Đến 5s Xong Thoát Ra Luôn Nhé. Ra Tới Sảnh AE Mới Bật Chức Năng Này... Nếu AE Vào Trận Có One Hit Và Bất Tử Rồi Thì Không Cần Bật Lại Nữa Nhé, Còn Nếu Không Có Thì AE Cứ Vào Trong Trận Rồi Bật Là Được.. Mình Đã Test Qua Mấy Bản Nhưng Có Bản Chỉ Cần Bật 1 Lần Là Sài Mãi, Có Bản Thì Mỗi Trận Bật 1 Lần, Vậy Nên AE Cứ Test Sao Cho Phù Hợp Với Phiên Bản Mà AE Sử Dụng Nhé].\n\n\n2. ༒Time Vip༒\n[Chức Năng Này AE Vào Trận Mới Bật Nhé, AE Đợi Qua 1s Rồi Bật Nha, Chức Năng Này Mỗi Trận Bật 1 Lần].\n\n\n3. ༒Skill Vip༒\n[Chức Năng Này Vào Trận Mới Bật Nhé, Nó Sẽ Buff Skill AE Chạy Vô Hạn, Nôm Na Là Vậy, Nói Chung Cũng Không Cần Thiết Bật Đâu, AE Muốn Thử Thì Cứ Thử Nhé, Chức Năng Này Mỗi Trận Nếu Nó Không Hoạt Động Thì Bật Lại].\n\n\n4. ༒Bí Cảnh༒\n[ Là Chức Năng Giữ Combo Liên Tục Khi AE đánh Bí Cảnh Nhé, Thường Thì AE Mà Không Giết Quái Liên Tục Nó Sẽ Mất Combo Liên Tục Thì Hack Này Sẽ Giữ Combo Cho AE Nhé, AE Giết Được Bao Nhiêu Quái Nó Sẽ + Dồn Combo Bằng Đấy Cho AE.\n Chức Năng Này Mỗi Trận Bật 1 Lần]>\n\n\n5. ༒Gia Tộc༒\n[ Chức Năng Này Giống Chức Năng Bí Cảnh Nhé.. Dùng Trong Map Gia Tộc...]\n\n\n6 ༒Ngư Ca Đàm༒\n[Chức Năng Này Là Hack Level Ngư Ca Đàm Nhé...]\nYêu Cầu Level 5 Trở Lên Để Kích Hoạt Được Full Nhé\nAE Điền Theo Tương Ứng Với Level Nhé:\n\nLVL1;   LVL2;   LVL3;   LVL4;       \n 6619252;3014771;6422625:113\n\n-> Ví Dụ:\n\n5;   10;   15;   20;       \n 6619252;3014771;6422625:113\n\n-> Xong Ấn Chạy Là Ok Nhé...\n\n\n\n\n7. ༒Speed X2༒\n[Chức Năng Này Sẽ Tăng Tốc Game Chạy x2, Chức Năng Này Mình Thấy Cũng Không Cần Thiết Lắm, AE Muốn Sài Thì Cứ Bật, Bật Ở Sảnh Nhé, Nên Bật Cuối Cùng].\n\n\n8. [À Ờ Thì AE Còn Chơi Game Gì Thì Có Thể ALO Mình Test Hack Nhé AE, Game Này Mình Còn Có Bản Mod APK Nữa, Không Cần Root Hay Chạy Qua Không Gian Ảo, Nhưng 1 Số Máy Lỗi Tí Kk, Hên Xui, Vậy Nhé AE Cần Hỗ Trợ Hay Muốn Nâng Cấp Gì Cứ Alo Mình. Cảm Ơn AE Đã Ủng Hộ!🔥].")
+end
+
+
+
+
+function F9()
 gg.copyText("0375574755")
-gg.alert("༒📲Đã Copy SĐT Admin📲༒\n\nHãy Vào Zalo Dán Vào Ô Tìm Kiếm Liên Hệ Với Admin Để Cấp File 📄 Mới!.\n\nXin Cảm Ơn!🇻🇳")
+gg.alert("༒📲Đã Copy SĐT Admin📲༒\n\nHãy Vào Zalo Dán Vào Ô Tìm Kiếm Để Liên Hệ Với Admin.\n\nXin Cảm Ơn!🇻🇳")
+end
+
+
+
+
+
+
+
+
+
+
+
+
+--❏━━━━━━━━━━━━━━❒
+function clear()
+gg.getResults(gg.getResultsCount())
+gg.clearResults()
+end
+function get()
+gg.getResults(gg.getResultsCount())
+end 
+function search()
+gg.getResults(gg.getResultsCount())
+gg.clearResults()
+gg.searchNumber(X,T) 
+end 
+function refine()
+gg.refineNumber(X,T) 
+end  
+function refinenot()
+gg.refineNumber(X,T,false,gg.SIGN_NOT_EQUAL) 
+end 
+function edit()
+gg.getResults(gg.getResultsCount())
+gg.editAll(X,T) 
+end 
+function check()
+E=nil E=gg.getResultsCount()
+end 
+function offset()
+O=tonumber(O) addoff=nil 
+addoff=gg.getResults(gg.getResultsCount())
+for i, v in ipairs(addoff) do
+addoff[i].address=addoff[i].address+O
+addoff[i].flags=T
+end
+gg.loadResults(addoff) 
+end 
+function error()
+gg.toast("🚫")
+end
+--❏━━━━━━━━━━━━━━❒
+function class()
+gg.clearResults()
+gg.setRanges(gg.REGION_OTHER | gg.REGION_C_ALLOC) 
+gg.searchNumber(":"..X,1)
+if gg.getResultsCount()==0 then E=0 return end 
+ItzMeu=nil ItzMeu=gg.getResults(1)
+gg.getResults(gg.getResultsCount())
+gg.refineNumber(tonumber(ItzMeu[1].value),1)
+ItzMeu=nil ItzMeu=gg.getResults(gg.getResultsCount())
+gg.clearResults()
+for i, v in ipairs(ItzMeu) do ItzMeu[i].address=ItzMeu[i].address-1 ItzMeu[i].flags=1 end
+ItzMeu=gg.getValues(ItzMeu) ItzMea={} ItzMeaa=1 
+for i, v in pairs(ItzMeu) do
+if ItzMeu[i].value==0 then
+ItzMea[ItzMeaa]={} ItzMea[ItzMeaa].address=ItzMeu[i].address ItzMea[ItzMeaa].flags=1 ItzMeaa=ItzMeaa+1 end end   
+if #(ItzMea)==0 then gg.clearResults() E=0 return end 
+u=nil
+for i, v in ipairs(ItzMea) do ItzMea[i].address=ItzMea[i].address+#(X)+1 ItzMea[i].flags=1 end  
+ItzMea=gg.getValues(ItzMea) ItzMes=nil ItzMes={} ItzMebb=1
+for i, v in ipairs(ItzMea) do
+if ItzMea[i].value==0 then ItzMes[ItzMebb]={} ItzMes[ItzMebb].address=ItzMea[i].address ItzMes[ItzMebb].flags=1 ItzMebb=ItzMebb+1 end end
+if #(ItzMes)==0 then gg.clearResults() E=0 return end 
+ItzMea=nil
+for i, v in ipairs(ItzMes) do ItzMes[i].address=ItzMes[i].address-#(X) ItzMes[i].flags=1 end
+gg.loadResults(ItzMes) 
+ItzMerange=nil ItzMerange=gg.getResults(gg.getResultsCount())
+ItzMerca=nil ItzMerca=0 ItzMero=nil ItzMero=0 ItzMera=nil 
+for i, v in ipairs(ItzMerange) do
+ItzMera=gg.getValuesRange(ItzMerange[i])
+if ItzMera.address=="Ca" then ItzMerca=1 end
+if ItzMera.address=="O" then ItzMero=1 end
+ItzMera=nil end 
+if ItzMerca==1 and ItzMero==0 then gg.setRanges(gg.REGION_C_ALLOC) end
+if ItzMerca==0 and ItzMero==1 then gg.setRanges(gg.REGION_OTHER) end 
+ItzMerca=nil ItzMero=nil ItzMera=nil 
+gg.searchPointer(0)
+if gg.getResultsCount()==0 then E=0 return end   
+ItzMeu=gg.getResults(gg.getResultsCount())
+gg.clearResults() 
+if gg.getTargetInfo().x64 then ItzMeo1=48 ItzMeo2=56 ItzMevt=32 else ItzMeo1=24 ItzMeo2=28 ItzMevt=4 end
+ERROR=0 
+::TRYAGAIN:: 
+ItzMey=nil ItzMey={} ItzMez=nil ItzMez={} 
+for i, v in ipairs(ItzMeu) do
+ItzMey[i]={} ItzMey[i].address=ItzMeu[i].address+ItzMeo1 ItzMey[i].flags=ItzMevt
+ItzMez[i]={} ItzMez[i].address=ItzMeu[i].address+ItzMeo2 ItzMez[i].flags=ItzMevt end 
+ItzMey=gg.getValues(ItzMey) ItzMez=gg.getValues(ItzMez) ItzMep=nil ItzMep={} ItzMexx=1
+for i, v in ipairs(ItzMey) do
+if ItzMey[i].value==ItzMez[i].value and #(tostring(ItzMey[i].value))>=8 then
+ItzMep[ItzMexx]=ItzMey[i].value
+ItzMexx=ItzMexx+1 end end
+ItzMexx=nil ItzMey=nil ItzMez=nil if #(ItzMep)==0 and ERROR==0 then 
+if gg.getTargetInfo().x64 then ItzMeo1=32 ItzMeo2=40 else ItzMeo1=16 ItzMeo2=20 end 
+ERROR=2 
+goto TRYAGAIN end    
+if #(ItzMep)==0 and ERROR==2 then E=0 return end
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.clearResults() ItzMexxx=1
+for i, v in ipairs(ItzMep) do
+gg.toast("[🔎]") 
+gg.searchNumber(tonumber(ItzMep[i]),ItzMevt)
+if gg.getResultsCount()~=0 then
+ItzMexx=nil ItzMexx=gg.getResults(gg.getResultsCount()) gg.clearResults()
+for ItzMeq=1,#(ItzMexx) do ItzMexx[ItzMeq].name="ITzMe" end
+gg.addListItems(ItzMexx) ItzMexxx=ItzMexxx+1
+end gg.clearResults() end 
+ItzMeu=nil ItzMep=nil ItzMexx=nil ItzMeq=nil 
+if ItzMexxx==1 then gg.clearResults() E=0 return end
+ItzMexxx=nil ItzMeload={} ItzMeremove={} ItzMexx=1
+ItzMeu=gg.getListItems()
+for i, v in ipairs(ItzMeu) do
+if ItzMeu[i].name=="ITzMe" then 
+ItzMeload[ItzMexx]={} ItzMeload[ItzMexx].address=ItzMeu[i].address+O ItzMeload[ItzMexx].flags=T
+ItzMeremove[ItzMexx]={} ItzMeremove[ItzMexx]=ItzMeu[i] ItzMexx=ItzMexx+1 end end 
+ItzMeload=gg.getValues(ItzMeload) gg.loadResults(ItzMeload) gg.removeListItems(ItzMeremove) end
+
+--❏━━━━━━━━━━━━━━❒
+while true do
+  if gg.isVisible(true)then
+    gg.setVisible(false)SMZ=1
+  end
+  if SMZ==1 then Menu()end
+end
+--❏━━━━━━━━━━━━━━❒
+
+
+		
+
