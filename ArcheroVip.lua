31054db7e4a44f35899d03ab751f8586a3a4d22f
Hoàng Nam Hải <147267934+Hoang-namhai@users.noreply.github.com>
2026-04-29 14:58:32 +0700

    Update ArcheroVip.lua

--- ArcheroVip.lua
+++ ArcheroVip.lua
@@ -11,35 +11,46 @@ end
 
 -- ================== MENU ==================
 on = '[✔]'; off = '[✖]'
-H1 = off
+H1,H2,H3,H4 = off,off,off,off
 local L = "libil2cpp.so"
 
 function Main()
     local m = gg.choice({
-        H1.." ᴏɴᴇʜɪᴛ test💥",        
-        "[𝐄𝐗𝐈𝐓]"
-  }, nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮G𝙰𝙼𝙴: 𝙰𝚁𝙲𝙷𝙴𝚁𝙾 𝚅𝙸𝙿\n💭V𝙴𝚁𝚂𝙸𝙾𝙽: 7.9.1 \n------------------------------------------------")
+        H1.." 𝙵𝚁𝙴𝙴 𝙰𝙳𝚂 🚫",
+        H2.." 𝚂𝙿𝙴𝙴𝙳 𝙱𝙰𝚃𝚃𝙻𝙴 💨", 
+        H3.." 𝙱𝚄𝙵𝙵 𝙴𝚇𝙿 𝙻𝙴𝚅𝙴𝙻 🗯️",
+        H4.." 𝙾𝙽𝙴𝙷𝙸𝚃 𝙶𝙾𝙳𝙼𝙾𝙳 💥",
+        "[𝙴𝚇𝙸𝚃]"
+  }, nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮G𝙰𝙼𝙴: 𝙰𝚁𝙲𝙷𝙴𝚁𝙾 𝚅𝙸𝙿\n💭V𝙴𝚁𝚂𝙸𝙾𝙽: 𝟽.𝟿.𝟷 \n------------------------------------------------")
     
     if m == 1 then H1=(H1==on) and off or on; OnOff1()
-    elseif m == 2 then 
+    elseif m == 2 then H2=(H2==on) and off or on; OnOff2()
+    elseif m == 3 then H3=(H3==on) and off or on; OnOff3()
+    elseif m == 4 then H4=(H4==on) and off or on; OnOff4()
+    elseif m == 5 then 
     gg.toast('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+    gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+    print('☞♥Zᴀʟᴏ: 0375574755♥☜')
     os.exit() end
 end
 
 --➤ ==================================== 
 function OnOff1()
     if H1 == on then
+
         local r = searchValue({
             "1179403647",
-            {706675681, 0x4f6a42c}
+            {-1447143426, 0x5AD2C64}
         }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
 
         if #r > 0 then
-            applyEdit(r, 0x4f6a42c, gg.TYPE_DWORD, 310820864)
-            applyEdit(r, 0x4F6A430, gg.TYPE_DWORD, 1923576640)
-            applyEdit(r, 0x4F6A434, gg.TYPE_DWORD, -698416192)
-
-            gg.toast('✅ Teast✅')
+            applyEdit(r, 0x5AD2C64, gg.TYPE_DWORD, -763363296)
+            applyEdit(r, 0x5AD2C68, gg.TYPE_DWORD, -698416192)
+            
+            applyEdit(r, 0x5AD2D04, gg.TYPE_DWORD, -763363296)
+            applyEdit(r, 0x5AD2D08, gg.TYPE_DWORD, -698416192)
+            
+            gg.toast('✅𝙵𝚁𝙴𝙴 𝙰𝙳𝚂✅')
             gg.clearResults()
             gg.clearList()
         else
@@ -51,15 +62,17 @@ function OnOff1()
 
         local r = searchValue({
             "1179403647",
-            {310820864, 0x4f6a42c}
+            {-763363296, 0x5AD2C64}
         }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
 
         if #r > 0 then
-            applyEdit(r, 0x4f6a42c, gg.TYPE_DWORD, 706675681)
-            applyEdit(r, 0x4F6A430, gg.TYPE_DWORD, -1440807966)
-            applyEdit(r, 0x4F6A434, gg.TYPE_DWORD, 335546990)
+            applyEdit(r, 0x5AD2C64, gg.TYPE_DWORD, -1447143426)
+            applyEdit(r, 0x5AD2C68, gg.TYPE_DWORD, -1459531788)
             
-            gg.toast('❌ Teast❌')
+            applyEdit(r, 0x5AD2D04, gg.TYPE_DWORD, -132247554)
+            applyEdit(r, 0x5AD2D08, gg.TYPE_DWORD, -1459531788)
+            
+            gg.toast('❌𝙵𝚁𝙴𝙴 𝙰𝙳𝚂❌')
             gg.clearResults()
             gg.clearList()
         else
@@ -69,7 +82,147 @@ function OnOff1()
         end
         end
 
+--➤ ==================================== 
+function OnOff2() 
+    if H2 == on then
+        local r = searchValue({
+            "1179403647",
+            {-65204248, 0x58C8A44}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
 
+        if #r > 0 then
+            applyEdit(r, 0x58C8A44, gg.TYPE_DWORD, 1384120320)
+            applyEdit(r, 0x58C8A48, gg.TYPE_DWORD, 1923621888)
+            applyEdit(r, 0x58C8A4C, gg.TYPE_DWORD, 505872384)
+            applyEdit(r, 0x58C8A50, gg.TYPE_DWORD, -698416192)
+            
+            gg.toast('✅𝚂𝙿𝙴𝙴𝙳✅')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+         --   H1 = off  -- Tự động tắt nút nếu không tìm thấy
+        end        
+        
+    else
+
+        local r = searchValue({
+            "1179403647",
+            {1384120320, 0x58C8A44}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
+
+        if #r > 0 then
+            applyEdit(r, 0x58C8A44, gg.TYPE_DWORD, -65204248)
+            applyEdit(r, 0x58C8A48, gg.TYPE_DWORD, -1459529730)
+            applyEdit(r, 0x58C8A4C, gg.TYPE_DWORD, -1459466252)
+            applyEdit(r, 0x58C8A50, gg.TYPE_DWORD, -1342060941)
+
+            gg.toast('❌𝚂𝙿𝙴𝙴𝙳❌')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+        --    H1 = on
+        end
+        end
+        end
+
+--➤ ==================================== 
+function OnOff3()
+    if H3 == on then
+        local r = searchValue({
+            "1179403647",
+            {-65204248, 0x4F68E3C}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
+
+        if #r > 0 then
+            applyEdit(r, 0x4F68E3C, gg.TYPE_DWORD, 310902752)
+            applyEdit(r, 0x4F68E40, gg.TYPE_DWORD, 1923660608)
+            applyEdit(r, 0x4F68E44, gg.TYPE_DWORD, 505872384)
+            applyEdit(r, 0x4F68E48, gg.TYPE_DWORD, -698416192)
+
+            gg.toast('✅𝙱𝚄𝙵𝙵 𝙴𝚇𝙿✅')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+         --   H1 = off  -- Tự động tắt nút nếu không tìm thấy
+        end        
+        
+    else
+
+        local r = searchValue({
+            "1179403647",
+            {310902752, 0x4F68E3C}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
+
+        if #r > 0 then
+            applyEdit(r, 0x4F68E3C, gg.TYPE_DWORD, -65204248)
+            applyEdit(r, 0x4F68E40, gg.TYPE_DWORD, -117438466)
+            applyEdit(r, 0x4F68E44, gg.TYPE_DWORD, -1459529738)
+            applyEdit(r, 0x4F68E48, gg.TYPE_DWORD, -1459466252)
+            
+            gg.toast('❌𝙱𝚄𝙵𝙵 𝙴𝚇𝙿❌')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+        --    H1 = on
+        end
+        end
+        end
+
+--➤ ==================================== 
+function OnOff4() 
+    if H4 == on then
+                local r = searchValue({
+            "1179403647",
+            {-788446209, 0x5904e60}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
+
+        if #r > 0 then
+            applyEdit(r, 0x5904e60, gg.TYPE_DWORD, 1386217440)
+            applyEdit(r, 0x5904E64, gg.TYPE_DWORD, 1924136928)
+            applyEdit(r, 0x5904E68, gg.TYPE_DWORD, -698416192)
+            
+            applyEdit(r, 0x5905134, gg.TYPE_DWORD, 1386217440)
+            applyEdit(r, 0x5905138, gg.TYPE_DWORD, 1924136928)
+            applyEdit(r, 0x590513C, gg.TYPE_DWORD, -698416192)
+            
+            gg.toast('✅𝙷𝙰𝙲𝙺 𝙾𝙽✅')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+         --   H1 = off  -- Tự động tắt nút nếu không tìm thấy
+        end        
+        
+    else
+
+        local r = searchValue({
+            "1179403647",
+            {1386217440, 0x5904e60}
+        }, gg.REGION_C_DATA | gg.REGION_CODE_APP, gg.TYPE_DWORD)
+
+        if #r > 0 then
+            applyEdit(r, 0x5904e60, gg.TYPE_DWORD, -788446209)
+            applyEdit(r, 0x5904E64, gg.TYPE_DWORD, -1459525634)
+            applyEdit(r, 0x5904E68, gg.TYPE_DWORD, -1459462152)
+            
+            applyEdit(r, 0x5905134, gg.TYPE_DWORD, -788446209)
+            applyEdit(r, 0x5905138, gg.TYPE_DWORD, -1459525634)
+            applyEdit(r, 0x590513C, gg.TYPE_DWORD, -1459462152)
+            gg.toast('❌𝙷𝙰𝙲𝙺 𝙾𝙵𝙵❌')
+            gg.clearResults()
+            gg.clearList()
+        else
+            gg.alert("❌ Không tìm thấy địa chỉ phù hợp!")
+        --    H1 = on
+        end
+        end
+        end
+    
+    
     
     
     
